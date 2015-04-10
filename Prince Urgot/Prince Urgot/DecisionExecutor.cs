d46e0155ed145da38a9d67984c19c5353a4636ca
ipom2 <timewayner9@gmail.com>
2015-04-10 14:46:11 -0400

    Update DecisionExecutor.cs

--- Prince Urgot/Prince Urgot/DecisionExecutor.cs
+++ Prince Urgot/Prince Urgot/DecisionExecutor.cs
@@ -1,5 +1,4 @@
-
-﻿using System;
+using System;
 using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
@@ -15,8 +14,6 @@ namespace Prince_Urgot
 
         public DecisionExecutor(Menu comboMenu)
         {
-            ComboMenu = comboMenu;
-            Menu();
             Game.OnUpdate += Game_OnGameUpdate;
         }
 
@@ -31,65 +28,5 @@ namespace Prince_Urgot
             if (Program.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
                 Combo();
         }
-        #region Menu
-        internal static void Menu()
-        {
-            ComboMenu.AddSubMenu(new Menu("Combo", "combo"));
-
-            ComboMenu.SubMenu("combo").AddItem(new MenuItem("useQ", "Use Q")).SetValue(true);
-            ComboMenu.SubMenu("combo").AddItem(new MenuItem("useW", "Use W")).SetValue(true);
-            ComboMenu.SubMenu("combo").AddItem(new MenuItem("useE", "Use E")).SetValue(true);
-            ComboMenu.SubMenu("combo").AddItem(new MenuItem("useR", "Use R (Auto R under Tower)")).SetValue(true);
-            ComboMenu.SubMenu("combo").AddItem(new MenuItem("preE", "HitChance E").SetValue(new StringList((new[] { "Low", "Medium", "High", "Very High" }))));
-
-            ComboMenu.AddSubMenu(new Menu("Misc", "misc"));
-            ComboMenu.SubMenu("misc").AddItem(new MenuItem("autoInt", "Interrupt with " + "R").SetValue(false));
-            ComboMenu.SubMenu("misc").AddItem(new MenuItem("packetcast", "Use PacketCast")).SetValue(true);
-        }
-        #endregion
-
-        #region Interrupter
-        private static void Interrupter_OnPossibleToInterrupt (Obj_AI_Base unit, Interrupter2.InterruptableTargetEventArgs args)
-        {
-            if (SpellClass.R.IsReady() && unit.IsEnemy && SpellClass.R.IsInRange(unit) && ComboMenu.Item("autoInt").GetValue<bool>())
-            {
-                SpellClass.R.CastOnUnit(unit, PacketCast);
-            }
-        }
-        #endregion
-
-        #region Combo
-        private static void Combo()
-        {
-            var target = TargetSelector.GetTarget(1200, TargetSelector.DamageType.Physical);
-            var castQ = (ComboMenu.Item("useQ").GetValue<bool>());
-            var castE = (ComboMenu.Item("useW").GetValue<bool>());
-            var castW = (ComboMenu.Item("useE").GetValue<bool>());
-            var castR = (ComboMenu.Item("useR").GetValue<bool>());
-
-            if (target == null)
-            {
-                return;
-            }
-            if (castE)
-            {
-                SpellE(target);
-            }
-            if (castQ)
-            {
-                SpellSecondQ();
-                SpellQ(target);
-            }
-            if (castW)
-            {
-                SpellW(target);
-            }
-            if (castR)
-            {
-                AutoR(target);
-            }
-            
-        }
-        #endregion
     }
 }
