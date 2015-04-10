bae3d197930d9a42a5198ae07e9e75092ccf9b9e
ipom2 <timewayner9@gmail.com>
2015-04-10 14:46:05 -0400

    Update DecisionMaker.cs

--- Prince Urgot/Prince Urgot/DecisionMaker.cs
+++ Prince Urgot/Prince Urgot/DecisionMaker.cs
@@ -1,1 +1,44 @@
+using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Prince_Urgot
+{
+	internal class DecisionMaker
+	{
+		public enum OrbwalkingMode
+        {
+            LastHit,
+            Mixed,
+            LaneClear,
+            Combo,
+            None
+        }
+        
+	    private DecisionExecutor DE;
+	    
+	    private static Obj_AI_Hero Player
+	    {
+	        get { return ObjectManager.Player; }
+	    }
+	
+	    public DecisionMaker(DecisionExecutor dE)
+	    {
+	    	DE = dE;
+	    	Game.OnUpdate += Game_OnGameUpdate;
+	    }
+	    
+	    public DecisionMaker()
+	    {
+	    	this(new DecisionExecutor());
+	    }
+	    
+	    
+	    private static void Game_OnGameUpdate(EventArgs args)
+	    {
+	    	DE.setDecision();
+	    }
+	}
+}
 
