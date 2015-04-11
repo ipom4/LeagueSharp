9cdd38cca8fcfb18dcfcf181430a551558066e09
ipom3 <timewayner8@gmail.com>
2015-04-11 19:08:13 -0400

    Update Program.cs

--- Prince Urgot/Prince Urgot/Program.cs
+++ Prince Urgot/Prince Urgot/Program.cs
@@ -73,12 +73,14 @@ namespace Prince_Urgot
                 Orbwalker.setMode(Orbwalking.OrbwalkingMode.Combo);
                 //Orbwalker.SetOrbwalkingPoint(minionChef.Position);//(target.Position - minionChef.Position)*0.5f+minionChef.Position);
                 Orbwalker.setMoveMode(OrbDancer.MoveModeType.WalkAround, minionChef, 10);
+                PrintChat(minionChef.Name);
             }
             else
             {
                 Orbwalker.setMode(Orbwalking.OrbwalkingMode.LaneClear);
                 //Orbwalker.SetOrbwalkingPoint(minionChef.Position);
                 Orbwalker.setMoveMode(OrbDancer.MoveModeType.WalkAround, minionChef, 10);
+                PrintChat(minionChef.Name);
             }
             
         }
